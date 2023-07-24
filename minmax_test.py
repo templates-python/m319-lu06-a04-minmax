d4797d9dc938b7bbcc5c739c4457c0d6acd5bcc0
Marcel Suter <marcel.suter@bzz.ch>
2023-07-24 13:03:03 +0200

    added minmax_test.py

---
+++ minmax_test.py
@@ -0,0 +1,17 @@
+from minmax import main
+
+
+def test_random(capsys):
+    numbers = main()
+    captured = capsys.readouterr()
+    assert captured.out == f'{min(numbers)}\n{max(numbers)}\n'
+
+
+def test_extended(capsys, monkeypatch):
+    inputs = iter([31])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    numbers = main()
+    assert len(numbers == 31)
+    captured = capsys.readouterr()
+    assert captured.out == f'{min(numbers)}\n{max(numbers)}\n'
+

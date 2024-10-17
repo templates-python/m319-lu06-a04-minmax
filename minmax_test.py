edaa0bc351e6736efe9179f8c214289a742a9faa
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-10-17 10:11:24 +0200

    fixed testcases

--- minmax_test.py
+++ minmax_test.py
@@ -1,17 +1,24 @@
 from minmax import main
 
 
-def test_random(capsys):
+def test_random(capsys, monkeypatch):
+    # Wir nutzen den monkeypatch, um die Benutzerangabe zu simulieren
+    inputs = iter([15])  # Simulieren, dass der Benutzer 15 Zahlen eingibt
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
     numbers = main()
     captured = capsys.readouterr()
+
+    assert len(numbers) == 15  # Sicherstellen, dass genau 15 Zahlen erstellt wurden
     assert captured.out == f'{min(numbers)}\n{max(numbers)}\n'
 
 
 def test_extended(capsys, monkeypatch):
-    inputs = iter([31])
+    inputs = iter([31])  # Simulieren, dass der Benutzer 31 Zahlen eingibt
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    numbers = main()
-    assert len(numbers) == 31
-    captured = capsys.readouterr()
-    assert captured.out == f'{min(numbers)}\n{max(numbers)}\n'
 
+    numbers = main()
+    captured = capsys.readouterr()
+
+    assert len(numbers) == 31  # Sicherstellen, dass genau 31 Zahlen erstellt wurden
+    assert captured.out == f'{min(numbers)}\n{max(numbers)}\n'

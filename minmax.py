ae333c9eccc356b243c1a5507c08c4a4908062dc
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-10-17 10:11:40 +0200

    better solution

--- minmax.py
+++ minmax.py
@@ -2,17 +2,31 @@ import random
 
 
 def main():
-    numbers = random.sample(range(-999, 999), 15)
-    smallest = 2147483647
-    biggest = -2147483648
+    # Akzeptiere die Eingabe des Benutzers für die Anzahl der Zufallszahlen
+    num_count = int(input('Anzahl Zufallszahlen: '))
+    # Basis Version
+    # num_count = 15
+
+    # Erzeuge eine Liste von Zufallszahlen zwischen -999 und 999
+    numbers = random.sample(range(-999, 1000), num_count)
+
+    # Definiere die Variablen 'smallest' und 'biggest' zum Finden der min/max-Werte
+    smallest = 10000  # Wert, der größer als der größte mögliche Zufallswert ist
+    biggest = -10000  # Wert, der kleiner als der kleinste mögliche Zufallswert ist
+
+    # Iteration über alle Zahlen in der Liste, um die kleinste und größte Zahl zu finden
     for number in numbers:
         if number < smallest:
             smallest = number
         if number > biggest:
             biggest = number
-    print(smallest)
-    print(biggest)
+
+    # Ausgabe der kleinsten und größten Zahl
+    print(f'{smallest}\n{biggest}')
+
+    # Rückgabe der Liste
     return numbers
+
 
 if __name__ == '__main__':
     main()
